99f66488dd04daea2bc8b99c902d3c13828ff1db
berzem1 <berzem1@users.noreply.github.com>
2025-11-23 10:03:56 +0000

    Add config.json, controller-validator-frontend.rs, dynamic.tsx

---
+++ frontend/web/src/controller-validator-frontend.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/artbid_fhe/frontend/web/src/controller-validator-frontend.rs
+// Generated at: 2025-12-03T23:45:44.387Z
+
+
---
+++ frontend/web/src/dynamic.tsx
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export const useserializer_account_payment = () => { return {}; };
+
+
