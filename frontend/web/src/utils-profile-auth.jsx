1f3af3dfc35ea0b01551aec162d0661217e6e69f
berzem1 <berzem1@users.noreply.github.com>
2025-11-23 13:33:42 +0000

    Add utils-profile-auth.jsx, validator-adaptive-components.js

---
+++ frontend/web/src/utils-profile-auth.jsx
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export const sms = () => { return null; };
+
+
---
+++ frontend/web/src/validator-adaptive-components.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function export() { return <div>Component</div>; }
+
+
